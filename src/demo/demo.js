256d28d5e78a72474419b6310412b999abc14fd5
Brion Vibber <brion@pobox.com>
2013-10-16 23:08:51 -0700

    Lift audio blacklist from Safari 6.1+
    Safari 6.0 has some sort of problem where it hangs when
    running the Vorbis audio decoding ... unless the debug
    console is open, which disables the JIT compiler. :P
    
    This doesn't happen in Safari 6.1 prerelease seed, which
    will hopefully soon be replacing 6.0 in peoples' software
    updates.
    
    Safari 7 on OS X 10.9 is also known good.

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -528,16 +528,16 @@
 			options = {};
 		
 		// Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1
-		if (navigator.userAgent.match(/Version\/6\.[0-9a-z.]+ Safari/)) {
-			// Something may be wrong with the JIT compiler in Safari 6;
+		if (navigator.userAgent.match(/Version\/6\.0\.[0-9a-z.]+ Safari/)) {
+			// Something may be wrong with the JIT compiler in Safari 6.0;
 			// when we decode Vorbis with the debug console closed it falls
 			// into 100% CPU loop and never exits.
 			//
 			// Blacklist audio decoding for this browser.
 			//
-			// Known working in Safari 7.
+			// Known working in Safari 6.1 and 7.
 			options.audio = false;
-			showStatus('Audio disabled due to bug on Safari 6');
+			showStatus('Audio disabled due to bug on Safari 6.0');
 		}
 		
 		codec = new OgvJs(options);
