b5162ff5681b1e57479b6c346862737c8b877885
Maik Merten <maikmerten@googlemail.com>
2014-02-13 19:44:23 +0100

    change sed parameters so it works with both BSD and GNU sed

--- compileOgg.sh
+++ compileOgg.sh
@@ -8,8 +8,8 @@ if [ ! -f configure ]; then
   
   # -O20 and -04 cause problems
   # see https://github.com/kripken/emscripten/issues/264
-  sed -i '' 's/-O20/-O2/g' configure
-  sed -i '' 's/-O4/-O2/g' configure
+  sed -i.bak 's/-O20/-O2/g' configure
+  sed -i.bak 's/-O4/-O2/g' configure
   
   # finally, run configuration script
   emconfigure ./configure
--- compileVorbis.sh
+++ compileVorbis.sh
@@ -6,16 +6,16 @@ cd libvorbis
 if [ ! -f configure ]; then
   # generate configuration script
   # disable running configure automatically
-  sed -i '' 's/$srcdir\/configure/#/' autogen.sh
+  sed -i.bak 's/$srcdir\/configure/#/' autogen.sh
   ./autogen.sh
   
   # -O20 and -04 cause problems
   # see https://github.com/kripken/emscripten/issues/264
-  sed -i '' 's/-O20/-O2/g' configure
-  sed -i '' 's/-O4/-O2/g' configure
+  sed -i.bak 's/-O20/-O2/g' configure
+  sed -i.bak 's/-O4/-O2/g' configure
   
   # disable oggpack_writealign test
-  sed -i '' 's/$ac_cv_func_oggpack_writealign/yes/' configure
+  sed -i.bak 's/$ac_cv_func_oggpack_writealign/yes/' configure
   
   # finally, run configuration script
   emconfigure ./configure --disable-oggtest --with-ogg=$dir/libogg --with-ogg-libraries=$dir/libogg/src/.libs
