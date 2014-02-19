8e79c057aca595cf6625b8808c0b4d7e1464f787
Brion Vibber <brion@pobox.com>
2014-02-19 01:44:22 -0800

    Reenable -O2 optimization with Relooper explicitly disabled
    Reduces ogv.js file size dramatically compared to -O1 but still
    decodes video slower... but doesn't hang on iOS arm64.

--- compileOgv.sh
+++ compileOgv.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # compile wrapper around libogg + libvorbis + libtheora
-# Using -O1 instead of -O2 to avoid some sort of optimization fail that breaks Vorbis decoding on arm64 iOS 7 devices
+# Using -O2 with relooper explicitly disabled to avoid a JIT hang on arm64 iOS devices in Vorbis decoding
 emcc \
-  -O1 \
+  -O2 \
   -s ASM_JS=1 \
   -s VERBOSE=1 \
   -s WARN_ON_UNDEFINED_SYMBOLS=1 \
   -s EXPORTED_FUNCTIONS="['_OgvJsInit', '_OgvJsDestroy', '_OgvJsReceiveInput', '_OgvJsProcess']" \
+  -s RELOOP=0 \
   -I libogg/include -Llibogg/src/.libs -logg \
   libogg/src/bitwise.o \
   -I libvorbis/include -Llibvorbis/lib/.libs -lvorbis \
