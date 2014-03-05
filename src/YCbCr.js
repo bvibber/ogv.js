b8c1fc2fbfe398b31d11caea5bd4839b58ee2500
Brion Vibber <brion@pobox.com>
2014-03-04 17:42:34 -0800

    Optimize YUV conversion for 4:2:2 and 4:2:0

--- src/YCbCr.js
+++ src/YCbCr.js
@@ -17,33 +17,81 @@
 			strideY = ybcbr.strideY,
 			strideCb = ybcbr.strideCb,
 			strideCr = ybcbr.strideCr;
-		processLine = function convertYCbCr_processLine_clamped(y) {
-			var xdec = 0,
-				ydec = y >> vdec,
-				YPtr = y * strideY,
-				CbPtr = ydec * strideCb,
-				CrPtr = ydec * strideCr,
-				outPtr = y * 4 * width,
-				colorY, colorCb, colorCr;
+		
+		if (hdec == vdec == 1) {
+			// Optimize for 4:2:0, which is most common
+			processLine = function convertYCbCr_processLine_420(y) {
+				var xdec = 0,
+					ydec = y >> vdec,
+					YPtr = y * strideY,
+					CbPtr = ydec * strideCb,
+					CrPtr = ydec * strideCr,
+					outPtr = y * 4 * width,
+					colorY0, colorY1, colorCb, colorCr,
+					multY0, multY1,
+					multCrR, multCbG, multCrG, multCbB;
 				
-			for (var x = 0; x < width; x++) {
-				xdec = x >> hdec;
-				colorY = bytesY[YPtr + x];
-				colorCb = bytesCb[CbPtr + xdec];
-				colorCr = bytesCr[CrPtr + xdec];
+				for (var x = 0; x < width; x += 2) {
+					xdec = x >> hdec;
+					colorY0 = bytesY[YPtr + x];
+					colorY1 = bytesY[YPtr + x + 1];
+					colorCb = bytesCb[CbPtr + xdec];
+					colorCr = bytesCr[CrPtr + xdec];
 	
-				// Quickie YUV conversion
-				// https://en.wikipedia.org/wiki/YCbCr#ITU-R_BT.2020_conversion
-				// multiplied by 256 for integer-friendliness
-				multY = (298 * colorY);
-				output[outPtr++] = (multY + (409 * colorCr) - 57088) >> 8;
-				output[outPtr++] = (multY - (100 * colorCb) - (208 * colorCr) + 34816) >> 8;
-				output[outPtr++] = (multY + (516 * colorCb) - 70912) >> 8;
-				output[outPtr++] = 255;
+					// Quickie YUV conversion
+					// https://en.wikipedia.org/wiki/YCbCr#ITU-R_BT.2020_conversion
+					// multiplied by 256 for integer-friendliness
+					multCrR = (409 * colorCr);
+					multCbG = (100 * colorCb);
+					multCrG = (208 * colorCr);
+					multCbB = (516 * colorCb);
+					
+					multY0 = (298 * colorY0);
+					output[outPtr++] = (multY0 + multCrR - 57088) >> 8;
+					output[outPtr++] = (multY0 - multCbG - multCrG + 34816) >> 8;
+					output[outPtr++] = (multY0 + multCbB - 70912) >> 8;
+					output[outPtr++] = 255;
+
+					multY1 = (298 * colorY1);
+					output[outPtr++] = (multY1 + multCrR - 57088) >> 8;
+					output[outPtr++] = (multY1 - multCbG - multCrG + 34816) >> 8;
+					output[outPtr++] = (multY1 + multCbB - 70912) >> 8;
+					output[outPtr++] = 255;
+				}
 			}
-		}
-		for (var y = 0; y < height; y++) {
-			processLine(y);
+			for (var y = 0; y < height; y++) {
+				processLine(y);
+			}
+		} else {
+			failout();
+			processLine = function convertYCbCr_processLine_clamped(y) {
+				var xdec = 0,
+					ydec = y >> vdec,
+					YPtr = y * strideY,
+					CbPtr = ydec * strideCb,
+					CrPtr = ydec * strideCr,
+					outPtr = y * 4 * width,
+					colorY, colorCb, colorCr;
+				
+				for (var x = 0; x < width; x++) {
+					xdec = x >> hdec;
+					colorY = bytesY[YPtr + x];
+					colorCb = bytesCb[CbPtr + xdec];
+					colorCr = bytesCr[CrPtr + xdec];
+	
+					// Quickie YUV conversion
+					// https://en.wikipedia.org/wiki/YCbCr#ITU-R_BT.2020_conversion
+					// multiplied by 256 for integer-friendliness
+					multY = (298 * colorY);
+					output[outPtr++] = (multY + (409 * colorCr) - 57088) >> 8;
+					output[outPtr++] = (multY - (100 * colorCb) - (208 * colorCr) + 34816) >> 8;
+					output[outPtr++] = (multY + (516 * colorCb) - 70912) >> 8;
+					output[outPtr++] = 255;
+				}
+			}
+			for (var y = 0; y < height; y++) {
+				processLine(y);
+			}
 		}
 	}
 })();
