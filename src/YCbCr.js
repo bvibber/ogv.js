94f91894838f81ddad0f517c2b7c74c54183be26
Brion Vibber <brion@pobox.com>
2014-03-07 08:15:55 -0800

    Optimize YUV output for 4:2:0 slightly more
    We were only doing one line at a time, but we can save a little
    math by doing two lines at a time.
    
    Shaves per-frame drawing time at 160p on iPod Touch from about
    10ms down to about 8ms.

--- src/YCbCr.js
+++ src/YCbCr.js
@@ -23,18 +23,22 @@
 			processLine = function convertYCbCr_processLine_420(y) {
 				var xdec = 0,
 					ydec = y >> vdec,
-					YPtr = y * strideY,
+					Y0Ptr = y * strideY,
+					Y1Ptr = Y0Ptr + strideY,
 					CbPtr = ydec * strideCb,
 					CrPtr = ydec * strideCr,
-					outPtr = y * 4 * width,
-					colorY0, colorY1, colorCb, colorCr,
-					multY0, multY1,
+					outPtr0 = y * 4 * width,
+					outPtr1 = outPtr0 + 4 * width,
+					colorY00, colorY01, colorY10, colorY11, colorCb, colorCr,
+					multY00, multY01, multY10, multY11,
 					multCrR, multCbG, multCrG, multCbB;
 				
 				for (var x = 0; x < width; x += 2) {
 					xdec = x >> hdec;
-					colorY0 = bytesY[YPtr + x];
-					colorY1 = bytesY[YPtr + x + 1];
+					colorY00 = bytesY[Y0Ptr + x];
+					colorY01 = bytesY[Y0Ptr + x + 1];
+					colorY10 = bytesY[Y1Ptr + x];
+					colorY11 = bytesY[Y1Ptr + x + 1];
 					colorCb = bytesCb[CbPtr + xdec];
 					colorCr = bytesCr[CrPtr + xdec];
 	
@@ -46,20 +50,32 @@
 					multCrG = (208 * colorCr);
 					multCbB = (516 * colorCb);
 					
-					multY0 = (298 * colorY0);
-					output[outPtr++] = (multY0 + multCrR - 57088) >> 8;
-					output[outPtr++] = (multY0 - multCbG - multCrG + 34816) >> 8;
-					output[outPtr++] = (multY0 + multCbB - 70912) >> 8;
-					output[outPtr++] = 255;
+					multY00 = (298 * colorY00);
+					output[outPtr0++] = (multY00 + multCrR - 57088) >> 8;
+					output[outPtr0++] = (multY00 - multCbG - multCrG + 34816) >> 8;
+					output[outPtr0++] = (multY00 + multCbB - 70912) >> 8;
+					output[outPtr0++] = 255;
 
-					multY1 = (298 * colorY1);
-					output[outPtr++] = (multY1 + multCrR - 57088) >> 8;
-					output[outPtr++] = (multY1 - multCbG - multCrG + 34816) >> 8;
-					output[outPtr++] = (multY1 + multCbB - 70912) >> 8;
-					output[outPtr++] = 255;
+					multY01 = (298 * colorY01);
+					output[outPtr0++] = (multY01 + multCrR - 57088) >> 8;
+					output[outPtr0++] = (multY01 - multCbG - multCrG + 34816) >> 8;
+					output[outPtr0++] = (multY01 + multCbB - 70912) >> 8;
+					output[outPtr0++] = 255;
+					
+					multY10 = (298 * colorY10);
+					output[outPtr1++] = (multY10 + multCrR - 57088) >> 8;
+					output[outPtr1++] = (multY10 - multCbG - multCrG + 34816) >> 8;
+					output[outPtr1++] = (multY10 + multCbB - 70912) >> 8;
+					output[outPtr1++] = 255;
+
+					multY11 = (298 * colorY11);
+					output[outPtr1++] = (multY11 + multCrR - 57088) >> 8;
+					output[outPtr1++] = (multY11 - multCbG - multCrG + 34816) >> 8;
+					output[outPtr1++] = (multY11 + multCbB - 70912) >> 8;
+					output[outPtr1++] = 255;
 				}
 			}
-			for (var y = 0; y < height; y++) {
+			for (var y = 0; y < height; y += 2) {
 				processLine(y);
 			}
 		} else {
