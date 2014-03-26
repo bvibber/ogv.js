d7e5a2ffba5c9d40e24969385e01f4906c7dd6fe
Maik Merten <maikmerten@googlemail.com>
2014-03-26 19:21:37 +0100

    basic compilation infrastructure for Opus

---
+++ compileOpusJs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dir=`pwd`
+
+# set up the build directory
+mkdir build
+cd build
+
+mkdir js
+cd js
+
+mkdir root
+mkdir libopus
+cd libopus
+  
+# finally, run configuration script
+emconfigure ../../../libopus/configure --disable-shared --disable-asm --disable-oggtest --disable-doc --disable-extra-programs --prefix="$dir/build/js/root"
+
+# compile libopus
+emmake make
+emmake make install
+
+cd ..
+cd ..
+cd ..
