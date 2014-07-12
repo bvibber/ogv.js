d6cc70eee0b09191102b5ee41a2c25e8df405e70
Brion Vibber <brion@pobox.com>
2014-07-11 22:59:12 -0700

    Fixes for emscripten SDK 1.21
    * error out instead of warning on empty symbols
    * switch from linking -lfoo to explicitly listing the library
    * switch from linking the static libraries to linking the shared libs
    
    For some reason emscripten has stopped liking linking to the static
    libraries... but of course it links shared libraries statically anyway.
    Ah, fun.

--- compileOgvJs.sh
+++ compileOgvJs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+suffix=so
+if [ `uname -s` == "Darwin" ]; then
+	suffix=dylib
+fi
+
 # compile wrapper around libogg + libvorbis + libtheora
 # Warning: -O2 enables the emscripten relooper which, on current release version hangs
 # on arm64 iOS devices in Vorbis decoding. Use with emscripten-fastcomp for iOS arm64!
@@ -7,14 +12,13 @@ EMCC_FAST_COMPILER=1 emcc \
   -O2 \
   -s ASM_JS=1 \
   -s VERBOSE=1 \
-  -s WARN_ON_UNDEFINED_SYMBOLS=1 \
+  -s ERROR_ON_UNDEFINED_SYMBOLS=1 \
   -s EXPORTED_FUNCTIONS="['_OgvJsInit', '_OgvJsDestroy', '_OgvJsReceiveInput', '_OgvJsProcess', '_OgvJsDecodeFrame', '_OgvJsDecodeAudio']" \
   -Ibuild/js/root/include \
   -Lbuild/js/root/lib \
-  -logg \
-  build/js/libogg/src/bitwise.o \
-  -lvorbis \
-  -ltheoradec \
+  build/js/root/lib/libogg.$suffix \
+  build/js/root/lib/libvorbis.$suffix \
+  build/js/root/lib/libtheoradec.$suffix \
   --js-library src/ogv-libs-mixin.js \
   src/ogv-libs.c \
   -o build/js/ogv-libs.js
