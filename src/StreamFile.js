398f46e93d4a870078cca0b06a9773b77736f549
Brion Vibber <brion@pobox.com>
2014-10-21 00:50:00 -0700

    Refactor StreamFile class

--- src/StreamFile.js
+++ src/StreamFile.js
@@ -20,146 +20,237 @@ function StreamFile(options) {
 		onread = options.onread || function(){},
 		ondone = options.ondone || function(){},
 		onerror = options.onerror || function(){},
-		bufferSize = options.bufferSize || 4096;
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url);
-	
-	var foundMethod = false;
-	function tryMethod(rt) {
-		if (foundMethod) {
-			return false;
-		}
-		try {
-			// Set the response type and see if it explodes!
-			xhr.responseType = rt;
-		} catch (e) {
-			// Safari 6 throws a DOM Exception on invalid setting
-			return false;
-		}
-		// Other browsers just don't accept the setting, so check
-		// whether it made it through.
-		if (xhr.responseType == rt) {
-			foundMethod = true;
-			return true;
-		}
-		return false;
-	}
-
-	var waitingForInput = false,
+		bufferSize = options.bufferSize || 4096,
+		seekPosition = 0,
+		//chunkSize = 1024 * 1024, // read/buffer up to a megabyte at a time
+		chunkSize = 0,
+		xhr = new XMLHttpRequest(),
+		waitingForInput = false,
 		doneBuffering = false,
-		bytesTotal = 0;
+		bytesTotal = 0,
+		bytesBuffered = 0,
+		bytesRead = 0;
 
-	function setBytesTotal(xhr) {
-		contentLength = xhr.getResponseHeader('Content-Length');
-		if (contentLength == null || contentLength == '') {
-			// Unknown file length... maybe streaming live?
-			bytesTotal = 0;
+	
+	// -- internal private methods
+	var internal = {
+
+		/**
+		 * Test if a given responseType value is valid on an XHR
+		 *
+		 * @return boolean
+		 */
+		tryMethod: function(rt) {
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", url);
+			try {
+				// Set the response type and see if it explodes!
+				xhr.responseType = rt;
+			} catch (e) {
+				// Safari 6 throws a DOM Exception on invalid setting
+				return false;
+			}
+			// Other browsers just don't accept the setting, so check
+			// whether it made it through.
+			return (xhr.responseType == rt);
+		},
+
+		setBytesTotal: function(xhr) {
+			var contentLength = xhr.getResponseHeader('Content-Length');
+			if (contentLength == null || contentLength == '') {
+				// Unknown file length... maybe streaming live?
+				bytesTotal = 0;
+			} else {
+				bytesTotal = parseInt(contentLength, 10);
+			}
+		},
+
+		openXHR: function() {
+			xhr = new XMLHttpRequest();
+			xhr.open("GET", url);
+
+			internal.setXHROptions(xhr);
+
+			var range = null;
+			if (seekPosition) {
+				range = 'bytes=' + seekPosition + '-';
+			}
+			if (chunkSize) {
+				range += (seekPosition + chunkSize - 1);
+			}
+			if (range !== null) {
+				xhr.setRequestHeader('Range', range);
+			}
+		
+			bytesBuffered = 0;
+			bytesRead = 0;
+
+			xhr.onreadystatechange = function(event) {
+				if (xhr.readyState == 2) {
+					internal.onXHRHeadersReceived(xhr);
+				} else if (xhr.readyState == 3) {
+					internal.onXHRLoading(xhr);
+				} else if (xhr.readyState == 4) {
+					// Complete.
+					internal.onXHRDone(xhr);
+				}
+			};
+
+			xhr.send();
+		},
+		
+		setXHROptions: function(xhr) {
+			throw new Error('abstract function');
+		},
+
+		onXHRHeadersReceived: function(xhr) {
+			if (xhr.status >= 400) {
+				// errrorrrrrrr
+				callback(null, "HTTP " + xhr.status + ": " +xhr.statusText);
+				onerror();
+				xhr.abort();
+			} else {
+				internal.setBytesTotal(xhr);
+				onstart();
+			}
+		},
+		
+		onXHRLoading: function(xhr) {
+			throw new Error('abstract function');
+		},
+		
+		onXHRDone: function(xhr) {
+			doneBuffering = true;
+		},
+		
+		dataToRead: function() {
+			throw new Error('abstract function');
+		},
+		
+		popBuffer: function() {
+			throw new Error('abstract function');
+		},
+
+		// Read the next binary buffer out of the buffered data
+		readNextChunk: function() {
+			if (waitingForInput) {
+				waitingForInput = false;
+				onread(internal.popBuffer());
+				if (doneBuffering && !internal.dataToRead()) {
+					// We're out of data!
+					ondone();
+				}
+			}
+		},
+
+	};
+
+	// -- Public methods
+	self.readBytes = function() {
+		if (internal.dataToRead()) {
+			var buffer = internal.popBuffer();
+			setTimeout(function() {
+				onread(buffer);
+			}, 0);
+		} else if (doneBuffering) {
+			// We're out of data!
+			setTimeout(function() {
+				ondone();
+			}, 0);
 		} else {
-			bytesTotal = parseInt(contentLength, 10);
+			// Nothing queued...
+			waitingForInput = true;
 		}
-	}
+	};
 
-	Object.defineProperty(this, 'bytesTotal', {
-		get: function _getBytesTotal() {
+	self.abort = function() {
+		xhr.abort();
+	};
+
+	// -- public properties
+	Object.defineProperty(self, 'bytesTotal', {
+		get: function() {
 			return bytesTotal;
 		}
 	});
+	
+	Object.defineProperty(self, 'bytesBuffered', {
+		get: function() {
+			return bytesBuffered;
+		}
+	});
 
-	if (tryMethod('moz-chunked-arraybuffer')) {
+	Object.defineProperty(self, 'bytesRead', {
+		get: function() {
+			return bytesRead;
+		}
+	});
+
+	// -- Backend selection and method overrides
+	if (internal.tryMethod('moz-chunked-arraybuffer')) {
 		console.log("Streaming input using moz-chunked-arraybuffer");
-		
+
+		internal.setXHROptions = function(xhr) {
+			xhr.responseType = 'moz-chunked-arraybuffer';
+
+			xhr.onprogress = function() {
+				// xhr.response is a per-chunk ArrayBuffer
+				var buffer = xhr.response;
+
+				if (buffer) {
+					bytesBuffered += buffer.byteLength;
+					onbuffer();
+					buffers.push(buffer);
+				
+					internal.readNextChunk();
+				}
+			};
+		};
+
 		var buffers = [];
 		
-		function popBuffer() {
+		internal.dataToRead = function() {
+			return (buffers.length > 0);
+		};
+
+		internal.popBuffer = function() {
 			var buffer = buffers.shift();
 			if (!bufferSize || bufferSize >= buffer.byteLength) {
-				self.bytesRead += buffer.byteLength;
+				bytesRead += buffer.byteLength;
 				return buffer;
 			} else {
 				// Split the buffer and requeue the rest
 				var thisBuffer = buffer.slice(0, bufferSize),
 					nextBuffer = buffer.slice(bufferSize);
 				buffers.unshift(nextBuffer);
-				self.bytesRead += thisBuffer.byteLength;
+				bytesRead += thisBuffer.byteLength;
 				return thisBuffer;
 			}
-		}
-		function handleInput(buffer) {
-			self.bytesBuffered += buffer.byteLength;
-			onbuffer();
-			buffers.push(buffer);
-			if (waitingForInput) {
-				waitingForInput = false;
-				onread(popBuffer());
-				if (doneBuffering && buffers.length == 0) {
-					// We're out of data!
-					ondone();
-				}
-			}
-		}
-
-		xhr.onreadystatechange = function(event) {
-			if (xhr.readyState == 2) {
-				if (xhr.status >= 400) {
-					// errrorrrrrrr
-					callback(null, "HTTP " + xhr.status + ": " +xhr.statusText);
-					onerror();
-					xhr.abort();
-				} else {
-					setBytesTotal(xhr);
-					onstart();
-				}
-			} else if (xhr.readyState == 4) {
-				// Complete.
-				doneBuffering = true;
-			}
 		};
 		
-		xhr.onprogress = function(event) {
-			// xhr.response is a per-chunk ArrayBuffer
-			handleInput(xhr.response);
+		internal.onXHRLoading = function(xhr) {
+			// we have to get from the 'progress' event
 		};
 		
-		self.readBytes = function() {
-			if (buffers.length > 0) {
-				var buffer = popBuffer();
-				// beware of tail recursion gone wrong
-				onread(buffer);
-			} else if (doneBuffering) {
-				// We're out of data!
-				setTimeout(function() {
-					ondone();
-				}, 0);
-			} else {
-				// Nothing queued...
-				waitingForInput = true;
-			}
-		};
-	}
-	
-	if (tryMethod('ms-stream')) {
+	} else if (internal.tryMethod('ms-stream')) {
 		// IE 10 supports returning a Stream from XHR.
 		console.log("Streaming input using MSStreamReader");
 		
+		// Don't bother reading in chunks, MSStream handles it for us
+		chunkSize = 0;
+		
 		var stream, streamReader;
 		
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 2) {
-				if (xhr.status >= 400) {
-					// errrorrrrrrr
-					callback(null, "HTTP " + xhr.status + ": " +xhr.statusText);
-					onerror();
-					xhr.abort();
-				}
-			} else if (xhr.readyState == xhr.LOADING) {
-				setBytesTotal(xhr);
-				// Transfer us over to the StreamReader...
-				stream = xhr.response;
-				xhr.onreadystatechange = null;
-				onstart();
-			}
-		}
+		internal.setXHROptions = function(xhr) {
+			xhr.responseType = 'ms-stream';
+		};
+		
+		internal.onXHRLoading = function(xhr) {
+			// Transfer us over to the StreamReader...
+			stream = xhr.response;
+			xhr.onreadystatechange = null;
+			onstart();
+		};
 		
 		self.readBytes = function() {
 			if (stream) {
@@ -168,8 +259,8 @@ function StreamFile(options) {
 					var buffer = event.target.result,
 						len = buffer.byteLength;
 					if (len > 0) {
-						self.bytesBuffered += len;
-						self.bytesRead += len;
+						bytesBuffered += len;
+						bytesRead += len;
 						onread(buffer);
 					} else {
 						// Zero length means end of stream.
@@ -184,20 +275,25 @@ function StreamFile(options) {
 				waitingForInput = true;
 			}
 		};
-	}
-	
-	if (!foundMethod && xhr.overrideMimeType !== undefined) {
-		foundMethod = true;
+
+	} else if (xhr.overrideMimeType !== undefined) {
 
 		// Use old binary string method since we can read reponseText
 		// progressively and extract ArrayBuffers from that.
 		console.log("Streaming input using XHR progressive binary string");
-		xhr.responseType = "text";
-		xhr.overrideMimeType('text/plain; charset=x-user-defined');
-	
-		var lastPosition = 0,
-			doneBuffering = false;
 		
+		internal.setXHROptions = function(xhr) {
+			xhr.responseType = "text";
+			xhr.overrideMimeType('text/plain; charset=x-user-defined');
+		};
+	
+		var lastPosition = 0;
+		
+		// Is there data available to read?
+		internal.dataToRead = function() {
+			return lastPosition < xhr.responseText.length;
+		};
+
 		// Is there a better way to do this conversion? :(
 		function stringToArrayBuffer(chunk) {
 			var len = chunk.length,
@@ -210,81 +306,23 @@ function StreamFile(options) {
 		}
 
 		// Return a buffer with up to bufferSize from the next data
-		function popBuffer() {
+		internal.popBuffer = function() {
 			var chunk = xhr.responseText.slice(lastPosition, lastPosition + bufferSize);
 			lastPosition += chunk.length;
-			self.bytesRead += chunk.length;
+			bytesRead += chunk.length;
 			return stringToArrayBuffer(chunk);
 		}
-
-		// Is there data available to read?
-		function dataToRead() {
-			return lastPosition < xhr.responseText.length;
-		}
 		
-		// Read the next binary buffer out of the input string
-		function readNextChunk() {
-			if (waitingForInput) {
-				waitingForInput = false;
-				onread(popBuffer());
-				if (doneBuffering && !dataToRead()) {
-					// We're out of data!
-					ondone();
-				}
-			}
-		}
-	
-		xhr.onreadystatechange = function(event) {
-			if (xhr.readyState == 2) {
-				if (xhr.status >= 400) {
-					// errrorrrrrrr
-					callback(null, "HTTP " + xhr.status + ": " +xhr.statusText);
-					onerror();
-					xhr.abort();
-				} else {
-					setBytesTotal(xhr);
-					onstart();
-				}
-			} else if (xhr.readyState == 3) {
-				// xhr.responseText is a binary string of entire file so far
-				self.bytesBuffered = xhr.responseText.length;
-				onbuffer();
-				readNextChunk();
-			} else if (xhr.readyState == 4) {
-				// Complete.
-				doneBuffering = true;
-			}
+		internal.onXHRLoading = function(xhr) {
+			// xhr.responseText is a binary string of entire file so far
+			bytesBuffered = xhr.responseText.length;
+			onbuffer();
+			internal.readNextChunk();
 		};
 		
-		self.readBytes = function() {
-			if (dataToRead()) {
-				var buffer = popBuffer();
-				setTimeout(function() {
-					onread(buffer);
-				}, 0);
-			} else if (doneBuffering) {
-				// We're out of data!
-				setTimeout(function() {
-					ondone();
-				}, 0);
-			} else {
-				// Nothing queued...
-				waitingForInput = true;
-			}
-		};
-	}
-	
-	if (!foundMethod) {
+	} else {
 		throw new Error("No streaming HTTP input method found.");
 	}
 	
-	xhr.send();
-	
-	self.bytesBuffered = 0;
-
-	self.bytesRead = 0;
-
-	self.abort = function() {
-		xhr.abort();
-	};
+	internal.openXHR();
 }
