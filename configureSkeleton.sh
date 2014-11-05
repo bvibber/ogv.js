27c776352e9e8434e7e0057d104e1882e7817cba
Brion Vibber <brion@pobox.com>
2014-11-05 10:49:22 -0800

    libskeleton's autogen needs an aclocal script from ogg
    set ACLOCAL_PATH before running its autogen.sh

--- configureSkeleton.sh
+++ configureSkeleton.sh
@@ -6,6 +6,6 @@ cd libskeleton
 if [ ! -f configure ]; then
   # generate configuration script
   sed -i.bak 's/$srcdir\/configure/#/' autogen.sh
-  ./autogen.sh
+  ACLOCAL_PATH="$dir/build/js/root/share/aclocal" ./autogen.sh
 fi
 cd ..
