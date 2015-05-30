d9d75642422a2e6aa5bb064c73f86d6bc3ebeea0
Brion Vibber <brion@pobox.com>
2015-05-30 07:12:55 -0700

    no longer needed (JS version used in JS build)

--- src/YCbCr.h
+++
@@ -1,5 +0,0 @@
-extern void convertYCbCr(unsigned char *bytesY, unsigned char *bytesCb, unsigned char *bytesCr,
-				  int strideY, int strideCb, int strideCr,
-				  int width, int height,
-				  int hdec, int vdec,
-				  unsigned char *output);
