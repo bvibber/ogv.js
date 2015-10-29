2f6c5b70b589c90ee612eb10e72a38b38ab019bf
Stephan <stephan@soundcloud.com>
2015-10-29 17:00:51 +0100

    no need for BogoSlow here

--- src/js/ogv.js
+++ src/js/ogv.js
@@ -4,17 +4,10 @@
 // Copyright (c) 2013-2015 Brion Vibber
 //
 
-require("./BogoSlow.js");
-
 var OGVCompat = require("./OGVCompat.js");
-
-// -- OGVPlayer.js
 var OGVPlayer = require("./OGVPlayer.js");
-
 var OGVWrapperCodec = require("./OGVWrapperCodec.js");
-
 var OGVDecoderVideoProxy = require("./OGVDecoderVideoProxy.js");
-
 var OGVDecoderAudioProxy = require("./OGVDecoderAudioProxy.js");
 
 module.exports = {
