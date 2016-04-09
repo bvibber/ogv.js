bf1e1fb2e0dcaf7da3d0f361c31713b13b415daa
Brion Vibber <brion@pobox.com>
2016-04-09 08:44:31 +0100

    re-export OGVMediaType to fix test cases
    will refactor all that stuff through one endpoint for 2.0 :D

--- src/js/ogv.js
+++ src/js/ogv.js
@@ -1,29 +1,20 @@
 //
 // -- ogv.js
 // https://github.com/brion/ogv.js
-// Copyright (c) 2013-2015 Brion Vibber
+// Copyright (c) 2013-2016 Brion Vibber
 //
 
-// Version 1.0 web-facing interfaces
+// Version 1.0's web-facing and test-facing interfaces
 window.OGVCompat = require("./OGVCompat.js");
 window.OGVLoader = require("./OGVLoader.js");
+window.OGVMediaType = require("./OGVMediaType.js");
 window.OGVPlayer = require("./OGVPlayer.js");
 window.OGVVersion = __OGV_FULL_VERSION__;
 
-/*
-var OGVWrapperCodec = require("./OGVWrapperCodec.js");
-var OGVDecoderVideoProxy = require("./OGVDecoderVideoProxy.js");
-var OGVDecoderAudioProxy = require("./OGVDecoderAudioProxy.js");
-*/
-
 module.exports = {
-	/*
-	DecoderVideoProxy: OGVDecoderVideoProxy,
-	DecoderAudioProxy: OGVDecoderAudioProxy,
-	WrapperCodec: OGVWrapperCodec,
-	*/
 	Compat: OGVCompat,
 	Loader: OGVLoader,
+	MediaType: OGVMediaType,
 	Player: OGVPlayer,
 	Version: __OGV_FULL_VERSION__
 };
