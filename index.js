ad988518d5e161b66d626a1bb38bb37317031262
Brion Vibber <brion@pobox.com>
2016-04-19 08:03:12 -0700

    Retool index.js to use webpack's file-loader
    Should make it easier to use ogv.js directly in a webpack-based
    project. Webpack's file-loader is used to require() the static
    assets from the dist dir, ensuring they appear under an 'ogv.js'
    subdir in the output, and the appropriate default path is set
    on the OGVLoader class.
    
    ```
    var ogvjs = require('ogvjs'),
        player = new ogvjs.OGVPlayer();
    player.src = '...';
    player.play();
    ```

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-module.exports = require('./dist/ogv.js');
+//
+// -- ogv.js
+// https://github.com/brion/ogv.js
+// Copyright (c) 2013-2016 Brion Vibber
+//
+// Webpack entry point for pre-built ogv.js distribution
+//
+// Uses file-loader to reference the various static assets
+// for the Flash shim and Web Worker modules.
+//
+// Bundles everything into ogvjs subdir, without changing filenames
+// as that would confuse the worker threads' loader.
+//
+// OGVLoader handles applying cache-breaking URL parameters, so hash
+// is not used in the output filename.
+
+var ogvjs = require('./dist/ogv.js'),
+  OGVLoader = ogvjs.OGVLoader,
+  moduleUrls = [
+    // AudioFeeder's Flash shim
+    require("file?name=ogv.js/[name].[ext]!./dist/dynamicaudio.swf"),
+
+    // Web Worker entry points
+    require("file?name=ogv.js/[name].[ext]!./dist/ogv-worker-audio.js"),
+    require("file?name=ogv.js/[name].[ext]!./dist/ogv-worker-video.js"),
+
+    // Audio decoders
+    require("file?name=ogv.js/[name].[ext]!./dist/ogv-decoder-audio-opus.js"),
+    require("file?name=ogv.js/[name].[ext]!./dist/ogv-decoder-audio-vorbis.js"),
+
+    // Video decoders
+    require("file?name=ogv.js/[name].[ext]!./dist/ogv-decoder-video-theora.js"),
+    require("file?name=ogv.js/[name].[ext]!./dist/ogv-decoder-video-vp8.js"),
+
+    // Demuxers
+    require("file?name=ogv.js/[name].[ext]!./dist/ogv-demuxer-ogg.js"),
+    require("file?name=ogv.js/[name].[ext]!./dist/ogv-demuxer-webm.js")
+  ];
+OGVLoader.base = moduleUrls[0].replace(/\/[^\/]+$/, '');
+
+module.exports = ogvjs;
