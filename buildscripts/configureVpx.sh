e06d153664990272772570aac159320b33f9e819
Brion Vibber <brion@pobox.com>
2016-09-18 01:58:01 -0700

    update libvpx to current

--- buildscripts/configureVpx.sh
+++ buildscripts/configureVpx.sh
@@ -3,9 +3,9 @@
 # configure libvpx
 dir=`pwd`
 cd libvpx
-if grep 'ARFLAGS = -rus' build/make/configure.sh; then
+if grep 'ARFLAGS = -crs' build/make/configure.sh; then
 
   # issue with llvm-ar; see https://code.google.com/p/webm/issues/detail?id=566
-  sed -i.bak 's/ARFLAGS = -rus/ARFLAGS = rus/' build/make/configure.sh
+  sed -i.bak 's/ARFLAGS = -crs/ARFLAGS = crs/' build/make/configure.sh
 fi
 cd ..
