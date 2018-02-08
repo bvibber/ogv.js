6cd6dbe1866d4e59cf7ecf09dd8bd21cc61381ac
Brion Vibber <brion@pobox.com>
2018-02-08 00:01:41 -0800

    simplify module instantiation, save a few bytes
    let the darn params through, was doing it anyway

--- src/js/modules/ogv-module-pre.js
+++ src/js/modules/ogv-module-pre.js
@@ -1,42 +1,9 @@
 /* global Module:true */
-/* global options:true */
-
-// Keep decoder options separate from emscripten module options
 var options = Module;
-Module = {
-	print: function(str) {
-		console.log(str);
-	}
-};
+
 if (typeof OGVLoader !== 'undefined') {
 	Module['pthreadMainPrefixURL'] = OGVLoader.base + '/';
 }
-if (options['memoryLimit']) {
+if (Module['memoryLimit']) {
 	Module['TOTAL_MEMORY'] = options['memoryLimit'];
 }
-// Hack to let parameters through for pthreads initialization
-for (option in options) {
-	if (options.hasOwnProperty(option)) {
-		Module[option] = options[option];
-	}
-}
-
-// hack for https://github.com/brion/ogv.js/issues/448
-var OGVDemuxerOgg = Module;
-var OGVDemuxerWebM = Module;
-var OGVDecoderVideoTheora = Module;
-var OGVDecoderVideoVP8 = Module;
-var OGVDecoderVideoVP9 = Module;
-var OGVDecoderAudioVorbis = Module;
-var OGVDecoderAudioOpus = Module;
-
-var OGVDecoderVideoVP8MT = Module;
-var OGVDecoderVideoVP9MT = Module;
-
-var OGVDemuxerOggW = Module;
-var OGVDemuxerWebMW = Module;
-var OGVDecoderVideoTheoraW = Module;
-var OGVDecoderVideoVP8W = Module;
-var OGVDecoderVideoVP9W = Module;
-var OGVDecoderAudioOpusW = Module;
-var OGVDecoderAudioVorbisW = Module;
