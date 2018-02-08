30e5d7576ea09be464bc13792bf53ac8e15a2d20
Brion Vibber <brion@pobox.com>
2018-02-08 13:38:22 -0800

    No longer necessary to strip the -O4 and -O20 used in some ogg bits
    emcc now caps to -O3 and warns but doesn't error

--- buildscripts/configureOgg.sh
+++ buildscripts/configureOgg.sh
@@ -7,10 +7,5 @@ if [ ! -f configure ]; then
   # generate configuration script
   sed -i.bak 's/$srcdir\/configure/#/' autogen.sh
   ./autogen.sh
-  
-  # -O20 and -04 cause problems
-  # see https://github.com/kripken/emscripten/issues/264
-  sed -i.bak 's/-O20/-O2/g' configure
-  sed -i.bak 's/-O4/-O2/g' configure
 fi
 cd ..
--- buildscripts/configureVorbis.sh
+++ buildscripts/configureVorbis.sh
@@ -9,11 +9,6 @@ if [ ! -f configure ]; then
   sed -i.bak 's/$srcdir\/configure/#/' autogen.sh
   ./autogen.sh
   
-  # -O20 and -04 cause problems
-  # see https://github.com/kripken/emscripten/issues/264
-  sed -i.bak 's/-O20/-O2/g' configure
-  sed -i.bak 's/-O4/-O2/g' configure
-  
   # disable oggpack_writealign test
   sed -i.bak 's/$ac_cv_func_oggpack_writealign/yes/' configure
 fi
