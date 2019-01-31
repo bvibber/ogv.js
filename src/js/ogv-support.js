edbc51d2fb74bc5e9c125e309d54dba61c5d5df8
Brion Vibber <brion@pobox.com>
2019-01-31 12:34:47 -0800

    fix

--- src/js/ogv-support.js
+++ src/js/ogv-support.js
@@ -4,11 +4,16 @@
 // Copyright (c) 2013-2019 Brion Vibber
 //
 
-export {OGVCompat} from './OGVCompat.js';
-export const OGVVersion = __OGV_FULL_VERSION__;
+import OGVCompat from './OGVCompat.js';
+const OGVVersion = __OGV_FULL_VERSION__;
 
 if (typeof window === 'object') {
     // 1.0-compat globals
     window.OGVCompat = OGVCompat;
     window.OGVVersion = OGVVersion;
 }
+
+export {
+    OGVCompat,
+    OGVVersion
+};
