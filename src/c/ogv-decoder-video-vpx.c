87ca12e7369a6bc654784be8d2f2a83eb5d633ec
Brion Vibber <brion@pobox.com>
2019-02-02 18:47:21 -0800

    allow up to 8 threads, for testing

--- src/c/ogv-decoder-video-vpx.c
+++ src/c/ogv-decoder-video-vpx.c
@@ -56,7 +56,7 @@ static void do_init() {
 
 	vpx_codec_dec_cfg_t cfg;
 #ifdef __EMSCRIPTEN_PTHREADS__
-	const int max_cores = 2;
+	const int max_cores = 8; // max threads for tiled decoding
 	int cores = emscripten_num_logical_cores();
 	if (cores > max_cores) {
 		cores = max_cores;
