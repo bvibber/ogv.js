a2c5237f2639992354a9caf0b0dd32ac1625ef65
Koen Klaren <koen.klaren@gmail.com>
2022-11-20 18:31:06 +0100

    Remove manual assignment of exports on `window` object
    This is now automatically handled by UMD when the scripts are loaded outside an AMD/CommonJS module environment.

--- src/js/ogv-support.js
+++ src/js/ogv-support.js
@@ -7,12 +7,6 @@
 import OGVCompat from './OGVCompat.js';
 const OGVVersion = __OGV_FULL_VERSION__;
 
-if (typeof window === 'object') {
-    // 1.0-compat globals
-    window.OGVCompat = OGVCompat;
-    window.OGVVersion = OGVVersion;
-}
-
 export {
     OGVCompat,
     OGVVersion
--- src/js/ogv-version.js
+++ src/js/ogv-version.js
@@ -6,9 +6,4 @@
 
 const OGVVersion = __OGV_FULL_VERSION__;
 
-if (typeof window === 'object') {
-    // 1.0-compat globals
-    window.OGVVersion = OGVVersion;
-}
-
 export {OGVVersion};
