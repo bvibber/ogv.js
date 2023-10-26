56662e6be14c9e25f177d996b2ab97e52742021f
Brion Vibber <brion@pobox.com>
2023-10-26 12:34:11 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //
 // -- ogv.js
 // https://github.com/brion/ogv.js
-// Copyright (c) 2013-2019 Brion Vibber
+// Copyright (c) 2013-2023 Brion Vibber
 //
 // Entry point for pre-built ogv.js distribution, can be pulled in
 // via webpack, browserify etc.
 //
 // You'll also need the static assets from the 'dist' subdirectory,
-// for the Flash audio shim and Web Worker modules. At runtime set:
+// for the codec and Web Worker modules. At runtime set:
 //
 //  require('ogv').OGVLoader.base = 'path/to/target-dir';
 //
